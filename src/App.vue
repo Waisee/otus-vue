2b749d1a409595d68c0b91cfb24c8639988cab26
waisee <waisee7@gmail.com>
2024-11-17 17:33:21 +0300

    Homework 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container d-flex justify-content-center mt-4">
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div v-if="!loading" class="row row-cols-4 g-4">
+      <Product 
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref } from 'vue';
+import Product from './components/Product.vue'
+
+const products = ref({});
+const loading = ref(true);
+
+fetch('https://fakestoreapi.com/products')
+  .then(response => {
+    if (response.status !== 200) {
+      console.log(response.status);
+
+      return;
+    }
+    response.json().then(json => {
+      products.value = json;
+      loading.value = false;
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  });
+</script>
---
+++ src/components/Product.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col">
+        <div class="card text-center">
+            <img :src="props.product.image" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">{{ props.product.title }}</h5>
+                <p class="card-text">{{ props.product.description }}</p>
+                <p class="card-text">Price: ${{ props.product.price }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    product: Object,
+});
+</script>
+
+<style scoped>
+.card {
+    width:18rem;
+}
+</style>
